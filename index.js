c43eaaa008cb6be747492bfcd1208acd13702401
loppujjy <jussi.je.loppukaarre@student.jyu.fi>
2020-08-23 13:22:30 +0300

    3.1
    Puhelinluettelon backend step 1

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": " 040-4005134",
+        "id": 1
+      },
+      {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+      },
+      {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+      },
+      {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+      },
+      {
+        "name": "Antti Isotalo",
+        "number": "+358-407009900",
+        "id": 5
+      },
+      {
+        "name": "Jaana Kemppainen",
+        "number": "050-5210421",
+        "id": 6
+      },
+      {
+        "name": "Donald Fauntleroy  Duck",
+        "number": "313-3133133",
+        "id": 7
+      },
+      {
+        "name": "Roope Ankka",
+        "number": "055-6790321",
+        "id": 8
+      }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+  })
+  
+  app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+  
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
