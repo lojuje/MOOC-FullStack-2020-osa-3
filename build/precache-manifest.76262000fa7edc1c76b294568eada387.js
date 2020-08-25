6e1f1d46448ad5acc243fa17f0d395012893d4de
loppujjy <jussi.je.loppukaarre@student.jyu.fi>
2020-08-25 13:40:02 +0300

    heroku commit for 3.11

---
+++ build/precache-manifest.76262000fa7edc1c76b294568eada387.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb3eae943502ff51543b1a7797dc3bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56cfb4c60080d135919c",
+    "url": "/static/css/main.88b9f277.chunk.css"
+  },
+  {
+    "revision": "d72cc7c3f59543947486",
+    "url": "/static/js/2.029bf66b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.029bf66b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56cfb4c60080d135919c",
+    "url": "/static/js/main.f15f3bd4.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
